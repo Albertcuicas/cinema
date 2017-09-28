c21b3c38dfbea4a135749aeda2eee6cbdb4e5842
Albertcuicas <Albertcuicas@hotmail.com>
2017-09-28 16:04:40 -0430

    agregar laravelCollective y fix frontend
    cambio de include de css y javascript

--- config/app.php
+++ config/app.php
@@ -145,6 +145,12 @@ return [
 		'Cinema\Providers\EventServiceProvider',
 		'Cinema\Providers\RouteServiceProvider',
 
+		/*
+         * Laravel Collective Service Providers...
+         */
+        'Collective\Html\HtmlServiceProvider::class',
+
+
 	],
 
 	/*
@@ -192,6 +198,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+		'Form' 		=> 'Collective\Html\FormFacade::class',
+        'Html' 		=> 'Collective\Html\HtmlFacade::class',
 
 	],
 
