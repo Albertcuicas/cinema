aa764193a59b72c4e1ee6b0bb9f8fc4fbd5da0f2
Albertcuicas <Albertcuicas@hotmail.com>
2017-09-27 21:27:17 -0430

    cambios modelos y migraciones

--- database/migrations/2017_09_27_204309_create_genres_table.php
+++ database/migrations/2017_09_27_204309_create_genres_table.php
@@ -15,6 +15,7 @@ class CreateGenresTable extends Migration {
 		Schema::create('genres', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('genre');
 			$table->timestamps();
 		});
 	}
