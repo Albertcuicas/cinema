92eb18c5aa7cee39dec6c5acf31b9ef4556a4dd2
Albertcuicas <Albertcuicas@hotmail.com>
2017-10-05 23:01:28 -0430

    Paginacion

--- app/Http/Requests/UserCreateRequest.php
+++ app/Http/Requests/UserCreateRequest.php
@@ -25,7 +25,7 @@ class UserCreateRequest extends Request
     {
         return [
             'name' => 'required',
-            'email' => 'required',
+            'email' => 'required|unique:users',
             'password' => 'required',
         ];
     }
