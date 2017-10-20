be30cc1d37151824744f2cb0d080b58f7408f885
Albertcuicas <Albertcuicas@hotmail.com>
2017-10-20 02:32:29 -0430

    Update con ajax
    creación de metodos edit y update en el crup; implementacion de modal
    para edicion y aplicación de cambios via Ajax

--- resources/views/genero/index.blade.php
+++ resources/views/genero/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.admin')
 	@section('content')
+	@include('genero.modal')
+		<div id="msj-success" class="alert alert-success alert-dismissible" role="alert" style="display:none">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+			<strong> Genero actualizado correctamente.</strong>
+		</div>
+
 		<table class="table">
 			<thead>
 				<th>Nombre</th>
